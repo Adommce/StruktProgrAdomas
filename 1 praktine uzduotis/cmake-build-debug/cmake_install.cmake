4da1fb601b080258d6c679116628dcaf797d297b
Adomas Paliulis <s056744@ad.viko.lt>
2024-09-20 09:48:14 +0300

    Pridejau nauja aplankala/uzduoti

--- 1 praktine uzduotis/cmake-build-debug/cmake_install.cmake
+++ 1 praktine uzduotis/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/s056744/Documents/StruktProgrAdomas/Sav darbai/Pazymiu vidurkis
+# Install script for directory: C:/Users/s056744/Documents/StruktProgrAdomas/1 praktine uzduotis
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/StruktProgrAdomas")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/1_praktine_uzduotis")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/s056744/Documents/StruktProgrAdomas/Sav darbai/Pazymiu vidurkis/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/s056744/Documents/StruktProgrAdomas/1 praktine uzduotis/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ 1 praktine uzduotis/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int EUR, USDT, GBP, INR;
+
+
+
+
+
+    return 0;
+}
